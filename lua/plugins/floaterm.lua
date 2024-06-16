da63be3311da8d3fcf0a649ced5d902ae1cf9349
lasorda <7352092+lasorda@users.noreply.github.com>
2024-06-16 11:40:28 +0800

    Update floaterm.lua

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -5,6 +5,7 @@ function M.config()
     vim.g.floaterm_keymap_new = '<F7>'
     vim.g.floaterm_keymap_next = '<F8>'
     vim.g.floaterm_opener = 'edit'
+    vim.cmd('highlight FloatermBorder guibg=#1E1E1E guifg=#ffffff')
     vim.cmd('autocmd TermOpen * setlocal nonumber norelativenumber')
     vim.g.floaterm_autoclose = 0
 end
