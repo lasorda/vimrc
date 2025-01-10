c0d4f4b974798676777bf89d43ab56364f7d1d56
zhihaopan <zhihaopan@tencent.com>
2025-01-10 17:42:12 +0800

    up

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -4,7 +4,10 @@ function M.config()
     vim.g.floaterm_keymap_toggle = '<F6>'
     vim.g.floaterm_keymap_new = '<F7>'
     vim.g.floaterm_keymap_next = '<F8>'
+    vim.g.floaterm_wintype = 'split'
+    vim.g.floaterm_height = 0.3
     vim.g.floaterm_opener = 'edit'
+    vim.g.floaterm_position = 'botright'
     vim.cmd('highlight FloatermBorder guibg=#1E1E1E guifg=#ffffff')
     vim.cmd('autocmd TermOpen * setlocal nonumber norelativenumber')
     vim.g.floaterm_autoclose = 0
--- lua/plugins/rainbow.lua
+++ lua/plugins/rainbow.lua
@@ -3,12 +3,13 @@ local M = {}
 function M.config()
     vim.g.rainbow_active = 1
     vim.g.rainbow_conf = {
-        ctermfgs = {'DarkBlue', 'White', 'DarkCyan', 'DarkGray', 'DarkMagenta', 'DarkYellow', 'DarkGreen', 'DarkRed'},
+        ctermfgs = { 'White', 'DarkBlue', 'DarkCyan', 'DarkYellow', 'DarkGreen', 'DarkRed', 'DarkGray' },
         operators = '_,|+|-|*|/|===|!==_',
         parentheses = {
             'start=/(/ end=/)/ fold',
             'start=/\\[/ end=/\\]/ fold',
             'start=/{/ end=/}/ fold',
+            'start=/<\\ze\\w/ end=/>/ fold',
         }
     }
 end
